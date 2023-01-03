2a49821aa088f07f5312bf70cea8755e6ae50803
jagLas <jglasher@gmail.com>
2023-01-03 10:06:37 -0500

    finish phase 3

--- phase-3.js
+++ phase-3.js
@@ -6,10 +6,47 @@ function addNums10Timing(increment) {
 
   // Your code here
 
+  function addNums10(increment) {
+    const nums = [];
+    for (let i = increment; i <= 10 * increment; i += increment) {
+      console.time(`addNums(${i})`);
+      nums.push(addNums(i));
+      console.timeEnd(`addNums(${i})`);
+    }
+  
+    return nums;
+  }
+
+  const start = Date.now();
+  addNums10(increment);
+  const end = Date.now();
+
+  console.log(`${end - start} ms`);
+
 }
 
 
 function addManyNums10Timing(increment) {
+
+  function addManyNums10(increment) {
+
+    const nums = [];
+    for (let i = increment; i <= 10 * increment; i += increment) {
+      console.time(`addManyNums(${i})`)
+      nums.push(addManyNums(i));
+      console.timeEnd(`addManyNums(${i})`)
+    }
+  
+    return nums;
+  
+  }
+
+  const start = Date.now();
+  addManyNums10(increment)
+  const end = Date.now();
+
+  console.log(`${end - start} ms`);
+ 
 // Copy your `addManyNums10` code here
 // Then, add timing code
 
@@ -24,6 +61,6 @@ addNums10Timing(1000000);
 
 console.log("\n***********\n");
 
-n = 1000
+n = 5000
 console.log(`addManyNums(${n}): `);
 addManyNums10Timing(5000);
