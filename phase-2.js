4ff85e7f4b773eb63e9c2c29c2b54d57fe3be7bb
jagLas <jglasher@gmail.com>
2023-01-03 09:39:10 -0500

    finished phase 2

--- phase-2.js
+++ phase-2.js
@@ -2,13 +2,23 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
+  const nums = [];
+  for (let i = increment; i <= 10 * increment; i += increment) {
+    nums.push(addNums(i));
+  }
 
+  return nums;
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
+
+  const nums = [];
+  for (let i = increment; i <= 10 * increment; i += increment) {
+    nums.push(addManyNums(i));
+  }
+
+  return nums;
 
 }
 
